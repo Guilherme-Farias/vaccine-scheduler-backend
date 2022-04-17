51b1847abd97243321a495a66d0f45b784da8425
Guilherme Farias <guicfarias11@gmail.com>
2022-04-17 01:38:30 -0300

    docs: add GET /appointments documentation

--- src/server/docs/paths/appointmentsPath.ts
+++ src/server/docs/paths/appointmentsPath.ts
@@ -36,4 +36,30 @@ export const appointmentsPath = {
       },
     },
   },
+  get: {
+    tags: ['Agendamentos'],
+    summary: 'API para listar agendamentos',
+    description: 'Sem descrição',
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'array',
+              items: {
+                $ref: '#/schemas/appointment',
+              },
+            },
+          },
+        },
+      },
+      204: {
+        description: 'Sucesso, mas sem dados para exibir',
+      },
+      500: {
+        $ref: '#/components/serverError',
+      },
+    },
+  },
 };

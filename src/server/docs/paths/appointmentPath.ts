ea088cfe879a4dba3f20c0c47a6f2284569a4db6
Guilherme Farias <guicfarias11@gmail.com>
2022-04-22 03:41:14 -0300

    docs: add GET /appointments/:id route

--- src/server/docs/paths/appointmentPath.ts
+++ src/server/docs/paths/appointmentPath.ts
@@ -1,4 +1,46 @@
 export const appointmentPath = {
+  get: {
+    tags: ['Agendamentos'],
+    summary: 'API para buscar um agendamento por id',
+    description:
+      'Para esta rota da API basta enviar como parâmetro de rota o **id** do agendamento.',
+    parameters: [
+      {
+        in: 'path',
+        name: 'id',
+        description: 'ID do agendamento a ser buscado',
+        required: true,
+        schema: {
+          type: 'string',
+        },
+      },
+    ],
+    responses: {
+      200: {
+        description: 'Encontrado',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/appointment',
+            },
+          },
+        },
+      },
+      404: {
+        description: 'Agendamento não encontrado',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/error',
+            },
+          },
+        },
+      },
+      500: {
+        $ref: '#/components/serverError',
+      },
+    },
+  },
   put: {
     tags: ['Agendamentos'],
     summary: 'API para atualizar um agendamento',

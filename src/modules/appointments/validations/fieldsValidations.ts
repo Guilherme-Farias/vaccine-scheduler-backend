6fc59706ea0327f941d39391ce14ed5f1e803dba
Guilherme Farias <guicfarias11@gmail.com>
2022-04-23 16:01:10 -0300

    fix: adjust fields validations

--- src/modules/appointments/validations/fieldsValidations.ts
+++ src/modules/appointments/validations/fieldsValidations.ts
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export const fieldsValidations = {
-  name: Joi.string().required().messages({
+  name: Joi.string().trim().required().messages({
     'string.base': '`name` deve ser do tipo `string`',
     'string.empty': 'O nome não pode ser vazio',
     'any.required': '`name` deve ser enviado',
@@ -9,13 +9,15 @@ export const fieldsValidations = {
   birth_date: Joi.date().iso().less('now').required().messages({
     'date.base': '`birth_date` deve ser do tipo `date`',
     'date.format': 'A data deve seguir padrão ISO',
-    'date.less': 'A data de nascimento deve ser menor do que o dia atual',
+    'date.less':
+      'A data de nascimento deve ser menor do que o dia atual e horário atual',
     'any.required': '`birth_date` deve ser enviado',
   }),
   appointment_date: Joi.date().iso().min('now').required().messages({
     'date.base': '`appointment_date` deve ser do tipo `date`',
     'date.format': 'A data deve seguir padrão ISO',
-    'date.min': 'A data do agendamento deve maior do que o dia atual',
+    'date.min':
+      'A data do agendamento deve maior do que o dia atual e horário atual',
     'any.required': '`appointment_date` deve ser enviado',
   }),
 };
